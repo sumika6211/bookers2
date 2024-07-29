0db7b8add4bba527e2e952c5c3cd79026d3e6044
sumika6211 <153085192+sumika6211@users.noreply.github.com>
2024-07-29 12:24:40 +0000

    7/29終了

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    post "users/guest_sign_in", to: "users#guest_sign_in"
+  end
   devise_for :users
   root to: 'homes#top'
   get "home/about" => "homes#about", as: "about"
-  resources :books, only: [:index, :show, :edit, :create, :update, :destroy]
-  resources :users, only: [:index, :show, :edit, :update]
-  devise_scope :user do
-    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+  resources :books, only: [:index, :show, :edit, :create, :update, :destroy] do
+    resource :favorite, only: [:create, :destroy]
   end
+  resources :users, only: [:index, :show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

da7baeb931b501de2c436cb567afee9c1652fdb4
sumika6211 <153085192+sumika6211@users.noreply.github.com>
2024-07-30 13:04:29 +0000

    7/30終了

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get "home/about" => "homes#about", as: "about"
   resources :books, only: [:index, :show, :edit, :create, :update, :destroy] do
-    resource :favorite, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
   end
   resources :users, only: [:index, :show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

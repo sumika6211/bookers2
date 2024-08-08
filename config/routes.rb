b0e282ca0da070769755bd4ff8dd0fdd9fb94422
sumika6211 <153085192+sumika6211@users.noreply.github.com>
2024-08-08 11:35:52 +0000

    8/8終了

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,6 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show, :edit, :update] do
     resource :relationships, only: [:create, :destroy, :show]
   end
+  get "search" => "searches#search"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

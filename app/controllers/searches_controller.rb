166988b7a73844997942279d978bfa29ebc5c9a2
sumika6211 <153085192+sumika6211@users.noreply.github.com>
2024-08-25 01:42:26 +0000

    8/25終了

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,10 +2,13 @@ class SearchesController < ApplicationController
   def search
     @word = params[:word]
     @range = params[:range]
+    @books = Book.all.includes(:tags)
     if @range == "User"
       @users = User.looks(params[:search], params[:word])
+    elsif @range == "Book"
+      @books = @books.looks(params[:search], params[:word])
     else
-      @books = Book.looks(params[:search], params[:word])
+      @books = @books.where("book_tags.tag.name": params[:tag_name])
     end
   end
 end
